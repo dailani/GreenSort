1f93e6fe5513b3136d45877119082767f1678fb4
leonlolly <>
2023-10-22 11:46:33 +0200

    adj get address

--- src/app/src/routes/+layout.svelte
+++ src/app/src/routes/+layout.svelte
@@ -2,13 +2,26 @@
 	import { onMount } from 'svelte';
 	import '../app.css';
 	import { Camera } from '@capacitor/camera';
+	import { getAddress, getCategory } from '$lib/backend/ai-controller';
 
 	onMount(() => {
 		Camera.requestPermissions();
 	});
+
+	async function captureImage(){
+		let a = await getAddress("Akkus");
+		try {
+			let a = await getAddress("material");
+			console.log('Category:' + a);
+		} catch (error) {
+			console.log("lol")
+			return;
+		}
+	}
 </script>
 
 <div class="flex flex-col w-full h-full">
+	<button on:click={captureImage} class="w-16 h-16 bg-gray-400 rounded-full" />
 	<main class="grow flex-1 min-h-0">
 		<slot />
 	</main>

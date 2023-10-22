66a9a841fa1d0429f473a9bac763929948e741cf
leonlolly <>
2023-10-22 11:48:26 +0200

    rec

--- src/app/src/routes/+layout.svelte
+++ src/app/src/routes/+layout.svelte
@@ -8,20 +8,9 @@
 		Camera.requestPermissions();
 	});
 
-	async function captureImage(){
-		let a = await getAddress("Akkus");
-		try {
-			let a = await getAddress("material");
-			console.log('Category:' + a);
-		} catch (error) {
-			console.log("lol")
-			return;
-		}
-	}
 </script>
 
 <div class="flex flex-col w-full h-full">
-	<button on:click={captureImage} class="w-16 h-16 bg-gray-400 rounded-full" />
 	<main class="grow flex-1 min-h-0">
 		<slot />
 	</main>

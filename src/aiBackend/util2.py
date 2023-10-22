adfc3c388077851e7e9a09622e43062ee655e598
leonlolly <>
2023-10-22 11:19:56 +0200

    change add to cat

--- src/aiBackend/util2.py
+++ src/aiBackend/util2.py
@@ -35,32 +35,6 @@ def getuser(user: str, password: str):
             connection.close()
             print("PostgreSQL connection is closed")
 
-def getuser(user: str, password: str):
-    # Database connection settings
-
-    # Initialize the connection outside the try block
-    connection = None
-
-    try:
-        # Connect to the database
-        connection = psycopg2.connect(**db_settings)
-
-        # Create a cursor
-        cursor = connection.cursor()
-        cursor.execute('SELECT * FROM "user" WHERE name = %s AND password = %s;', (user, password))
-        result = cursor.fetchone()
-        print(result)
-        cursor.close()
-        connection.close()
-        return result
-
-    except (Exception, psycopg2.Error) as error:
-        print("Error connecting to PostgreSQL:", error)
-    finally:
-        if connection:
-            cursor.close()
-            connection.close()
-            print("PostgreSQL connection is closed")
 
 def getadress(category: str):
     # Database connection settings
@@ -74,7 +48,7 @@ def getadress(category: str):
 
         # Create a cursor
         cursor = connection.cursor()
-        cursor.execute('SELECT * FROM "adress" WHERE category = %s;', (category))
+        cursor.execute('SELECT * FROM "categories" WHERE category = %s OR category_english = %s;', (category,category))
         result = cursor.fetchone()
         print(result)
         cursor.close()

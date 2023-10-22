a5e21f685311f6d7faa0a5fb7b671aedb33c835e
Dajlan Zaimaj <dajlan.zaimaj@inheaden.io>
2023-10-22 12:00:43 +0200

    summary

--- src/app/src/routes/summary/Summary.svelte
+++ src/app/src/routes/summary/Summary.svelte
@@ -1,16 +1,11 @@
 
 <script lang="ts">
-    	
+	import type { ImageMaterials } from '$lib/backend/ai-controller';
 	export let src: string;
     export let onBack: () => void;
     export let materials: ImageMaterials;
-    import trashcanImg from '$lib/static/recycle.svg'
-    import upArrow from '$lib/static/up-arrow.png'
-    
-	interface ImageMaterials {
-	materials: string[];
-	things: string[];
-}   
+    export let category: string;
+
 </script>
 
 <div on:click={onBack} class=" flex flex-col bg-[#1c0418] px-4 py-2 rounded-b-[48px] items-center justify-center gap-[1px]">
@@ -24,36 +19,32 @@
         </div>
           <div class="flex flex-col py-1 px-2  bg-[#16b78d] shadow-sm rounded-b-md">
             <div class="flex flex-col p-2 items-center">
-                <div class="text-md items-center text-white ">This items consists of</div>
+                <div class="text-sm items-center text-white ">This item's material consists of</div>
                 <div class=" text-2xl text-white font-bold flex flex-wrap text-clip">{materials == null ? 'Not Found' : materials.material}</div>
 
             </div>
-            <div class="flex flex-col p-2 items-center">
-                <div class="text-md items-center text-white ">Recycle this item and save</div>
-                <div class=" text-2xl text-white font-bold flex flex-wrap text-clip">+12</div>
-                <div class=" text-2xl text-white font-bold flex flex-wrap text-clip">Carbon Credits</div>
-            </div>
+        
         </div>
     </div>
     <div class="bg-[#16b78d] rounded-md shadow-md text-white py-2">
-        <div class="flex flex-col  items-center bg-inherit">
-            <img src={trashcanImg} alt="Recycling Trash Cans" class="bg-inherit max-h-16 ">
-        </div>
+    
         <div class="text-md items-center text-white text-center text-2xl">Disposal Information</div>
             <div class="flex flex-col gap-y-2 p-2">
             <div class="flex flex-row justify-between">
                 <div class="text-xl">Category</div>
-                <div class="text-xl">Yes</div>
+                <div class="text-xl">{category}</div>
             </div>
             <div class="flex flex-row justify-between">
                 <div class="text-xl">Reusable</div>
                 <div class="text-xl">Yes</div>
             </div>
-            <div class="flex flex-row justify-between">
-                <div class="text-xl">Location</div>
-                <div class="text-xl">small "redirect" icon</div>
-            </div>
+       
           
         </div>
     </div>
+
+    <div class=" bg-[#1c0418] rounded-md shadow-md text-white py-2">
+        <h1 class="font-bold text-md items-center text-white text-center text-2xl">How to Recycle</h1>
+    </div>
+    
 </div>
